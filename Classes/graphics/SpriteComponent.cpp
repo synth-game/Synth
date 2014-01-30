6c95e2ea77db4f29ac03ee11bf39ed1ed4fe1767
Chupee <julie.pronzac@gmail.com>
2014-01-30 18:06:34 +0100

    Add SpriteComponent

---
+++ Classes/graphics/SpriteComponent.cpp
@@ -0,0 +1,43 @@
+/* *****************************************************
+ *		SpriteComponent.cpp - @ Damaris Ankou - 30/01/14
+ ***************************************************** */
+
+#include "SpriteComponent.h"
+
+const char* SpriteComponent::COMPONENT_TYPE = "SpriteComponent";
+
+SpriteComponent::SpriteComponent()
+	: SynthComponent()
+	, _sFilePath()
+	, _pSprite(nullptr)
+	, _pParentLayer(nullptr) {
+
+}
+
+SpriteComponent::~SpriteComponent() {
+
+}
+
+bool SpriteComponent::init() {
+	SynthComponent::init(SpriteComponent::COMPONENT_TYPE);
+	_pSprite = Sprite::create(_sFilePath.c_str());
+	_pParentLayer->addChild(_pSprite);
+	return true;
+}
+
+SpriteComponent* SpriteComponent::create(std::string sFilePath, Layer* pParentLayer) {
+	SpriteComponent* pRet = new SpriteComponent();
+	if (pRet != nullptr) {
+		pRet->autorelease();
+		pRet->_sFilePath = sFilePath;
+		pRet->_pParentLayer = pParentLayer;
+		pRet->init();
+	} else { 
+		CC_SAFE_DELETE(pRet);
+	}
+	return pRet;
+}
+
+void SpriteComponent::initListeners() {
+
+}
