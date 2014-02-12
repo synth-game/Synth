3aeac50f8c58d19292dd627dcd5a442375f4774d
Chupee <julie.pronzac@gmail.com>
2014-02-12 18:44:03 +0100

    Clean SpriteComponent .h and .c

--- Classes/graphics/SpriteComponent.cpp
+++ Classes/graphics/SpriteComponent.cpp
@@ -1,32 +1,46 @@
+/*!
+ * \file SpriteComponent.h
+ * \brief Add a sprite image (fixed) to the actor.
+ * \author Chupee
+ * \date 12/02/2014
+ */
 #include "SpriteComponent.h"
+#include "events/EditMoveEvent.h"
 
 
-namespace graphics
-{
-char* SpriteComponent::COMPONENT_TYPE;
+namespace graphics {
 
-SpriteComponent::SpriteComponent()
-{
+const char* SpriteComponent::COMPONENT_TYPE = "SpriteComponent";
+
+SpriteComponent::SpriteComponent() {
 
 }
 
-SpriteComponent::~SpriteComponent()
-{
+SpriteComponent::~SpriteComponent() {
 }
 
 bool SpriteComponent::init() {
-    return true;
+    SynthComponent::init(SpriteComponent::COMPONENT_TYPE);
+	return true;
 }
 
 SpriteComponent* SpriteComponent::create(std::string sSpriteName, Layer* pParent) {
-	return nullptr;
+	SpriteComponent* pRet = new SpriteComponent();
+    if (pRet != NULL && pRet->init()) {
+        pRet->autorelease();
+		pRet->_sSpriteName =			sSpriteName;
+		pRet->_pParent =				pParent;
+    } else {
+        CC_SAFE_DELETE(pRet);
+    }
+	return pRet;
 }
 
-void SpriteComponent::initListeners()
-{
+void SpriteComponent::initListeners() {
+	_pEditMoveEventListener = cocos2d::EventListenerCustom::create(events::EditMoveEvent::EVENT_NAME, CC_CALLBACK_1(SpriteComponent::onEditMove, this));
 }
 
-void SpriteComponent::onEditMove(EventCustom* pEvent)
-{
+void SpriteComponent::onEditMove(EventCustom* pEvent) {
 }
+
 }  // namespace graphics
--- Classes/graphics/SpriteComponent.h
+++ Classes/graphics/SpriteComponent.h
@@ -1,3 +1,9 @@
+/*!
+ * \file SpriteComponent.h
+ * \brief Add a sprite image (fixed) to the actor.
+ * \author Chupee
+ * \date 12/02/2014
+ */
 #ifndef GRAPHICS_SPRITE_COMPONENT_H
 #define GRAPHICS_SPRITE_COMPONENT_H
 
@@ -5,35 +11,18 @@
 #include "cocos2d.h"
 #include "Core/SynthComponent.h"
 
-namespace graphics
-{
-class SpriteComponent : public core::SynthComponent
-{
-protected:
-	std::string _sSpriteName;
+namespace graphics {
 
-	Sprite* _pSprite;
-
-	Layer* _pParent;
-
-	EventCustom* _pEditMoveEventListener;
+/*! \class SpriteComponent
+ * \brief Add a sprite image (fixed) to the actor.
+ *
+ * 
+ */
+class SpriteComponent : public core::SynthComponent {
 
 public:
-	static char* COMPONENT_TYPE;
-
-
-protected:
-	/**
-	 *
-	 */
-	SpriteComponent();
-
-	virtual void initListeners();
-
-public:
-	/**
-	 *
-	 *
+	/*
+	 * Methods
 	 */
 	~SpriteComponent();
 
@@ -44,10 +33,31 @@ public:
 	 */
 	static SpriteComponent* create(std::string sSpriteName, Layer* pParent);
 
-	/**
-	 *
-	 */
 	void onEditMove(EventCustom* pEvent);
+
+	/*
+	 * Members
+	 */
+	static const char* COMPONENT_TYPE;
+
+protected:
+	/*
+	 * Methods
+	 */
+	SpriteComponent();
+
+	virtual void initListeners();
+
+	/*
+	 * Members
+	 */
+	std::string _sSpriteName;
+
+	Sprite* _pSprite;
+
+	Layer* _pParent;
+
+	EventCustom* _pEditMoveEventListener;
 
 };
 
