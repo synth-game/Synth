24aaefe5e8e9b2f61d18b45c278f0a0936b94136
Jijidici <jdefaye.jijidici@gmail.com>
2014-02-20 15:49:29 +0100

    Add ActorType enum

---
+++ Classes/core/ActorType.h
@@ -0,0 +1,20 @@
+/*!
+ * \file ActorType.h
+ * \brief enum for each actor type
+ * \author Jijidici
+ * \date 20/02/2014
+ */
+#ifndef CORE_ACTOR_TYPE_H
+#define CORE_ACTOR_TYPE_H
+
+namespace core {
+typedef enum {
+	HERO,
+	FIREFLY,
+	LIGHT,
+	LIGHTSWITCH,
+	ACTORTYPE_COUNT
+} ActorType;
+
+}  // namespace core
+#endif
