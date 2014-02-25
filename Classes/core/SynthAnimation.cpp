ae120303ea1c4e93ca03fa4210531e069357a577
Chupee <julie.pronzac@gmail.com>
2014-02-25 12:38:33 +0100

    Add SynthAnimation in core.

---
+++ Classes/core/SynthAnimation.cpp
@@ -0,0 +1,18 @@
+/*!
+ * \file SynthAnimation.cpp
+ * \brief Base classe of Synth game objects
+ * \author Chupee
+ * \date 25/02/2014
+ */
+#include "SynthAnimation.h"
+
+namespace core {
+
+SynthAnimation::SynthAnimation(ActorType eType) {
+}
+
+SynthAnimation::~SynthAnimation() {
+
+}
+
+}  // namespace core
