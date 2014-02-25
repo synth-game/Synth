12407be76756c68119e1663380e370fbe638699d
Chupee <julie.pronzac@gmail.com>
2014-02-25 10:47:08 +0100

    Add ChangeStateEvent

---
+++ Classes/Events/ChangeStateEvent.cpp
@@ -0,0 +1,13 @@
+#include "ChangeStateEvent.h"
+
+namespace events {
+
+const char* ChangeStateEvent::EVENT_NAME = "ChangeStateEvent";
+
+ChangeStateEvent::ChangeIntensityEvent() {
+}
+
+ChangeStateEvent::~ChangeIntensityEvent() {
+}
+
+}  // namespace events
