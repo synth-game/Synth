4c88f7c783ae348bb8a15623235365db4af55c6b
Chupee <julie.pronzac@gmail.com>
2014-02-27 17:56:37 +0100

    Clean SoundComponent

--- Classes/Sounds/SoundComponent.cpp
+++ Classes/Sounds/SoundComponent.cpp
@@ -1,23 +1,42 @@
+/*!
+ * \file SoundComponent.cpp
+ * \brief The actor who have this component can make sounds or music
+ * \author Chupee
+ * \date 27/02/2014
+ */
 #include "SoundComponent.h"
 
-namespace sounds
-{
-char* SoundComponent::COMPONENT_TYPE;
+namespace sounds {
 
-SoundComponent::SoundComponent()
-{
+const char* SoundComponent::COMPONENT_TYPE = "SoundComponent";
+
+SoundComponent::SoundComponent() {
 }
 
-SoundComponent::~SoundComponent()
-{
+SoundComponent::~SoundComponent() {
 }
 
-void SoundComponent::onSetStateEvent(EventCustom* pEvent)
-{
-}
 
-bool SoundComponent::init()
-{
+bool SoundComponent::init() {
 	return false;
 }
+
+SoundComponent* SoundComponent::create() {
+	SoundComponent* pRet = new SoundComponent();
+    if (pRet != NULL && pRet->init()) {
+        pRet->autorelease();
+    } else {
+        CC_SAFE_DELETE(pRet);
+    }
+	return pRet;
+}
+
+void SoundComponent::initListeners() {
+
+}
+
+void SoundComponent::onSetStateEvent(EventCustom* pEvent) {
+}
+
+
 }  // namespace sounds
--- Classes/Sounds/SoundComponent.h
+++ Classes/Sounds/SoundComponent.h
@@ -1,44 +1,61 @@
+/*!
+ * \file SoundComponent.h
+ * \brief The actor who have this component can make sounds or music
+ * \author Chupee
+ * \date 27/02/2014
+ */
 #ifndef SOUNDS_SOUND_COMPONENT_H
 #define SOUNDS_SOUND_COMPONENT_H
 
 #include "Core/SynthComponent.h"
+#include "sounds/SoundType.h"
 
-namespace sounds
-{
-class SoundComponent : public core::SynthComponent
-{
-protected:
-	int _iTrackID;
+namespace sounds {
 
-	char* _state;
+/*! \class SoundComponent
+ * \brief The actor who have this component can make sounds or music
+ *
+ * 
+ */
+class SoundComponent : public core::SynthComponent {
 
 public:
-	static char* COMPONENT_TYPE;
 
+	/*! \brief Destructor */
+	~SoundComponent();
+
+	bool init();
+
+	/*! \brief Create the component */
+	static SoundComponent* create();
+
+	inline SoundType getTag() { return _eTag; }
+	inline char* getState() { return _sState; }
+
+	inline void setTag(SoundType eTag) { _eTag = eTag; }
+	inline void setState(char* sState) { _sState = sState; }
+
+	/*! \brief  Identifies the component */
+	static const char* COMPONENT_TYPE;
 
 protected:
-	/**
-	 *
-	 */
+
+	/*! \brief  Constructor */
 	SoundComponent();
 
-public:
-	/**
-	 *
-	 */
-	~SoundComponent();
+	/*! \brief  Init listeners */
+	void initListeners();
 
 	/**
 	 * - Regarde si l'état du owner correspond à un des agglomérat d'état de sa map
 	 * - Si c'est le cas, un son doit être joué ! On ajoute le tag du son dans la variable _nextSound si elle est vide.
-	 * Quand le son courant (tag dans _currentSound) est fini, on met le son de _nextSound dans _current sound et on le joue.
+	 * Quand le son courant (tag dans _currentSound) est fini, on met le son de _nextSound dans _current sound et on le joue. ==> whaaat
 	 */
 	void onSetStateEvent(EventCustom* pEvent);
 
-	/**
-	 *
-	 */
-	bool init();
+	SoundType _eTag;
+
+	char* _sState;
 
 };
 
