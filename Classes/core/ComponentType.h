f6b821f465770e5a04fbc097bf5953fbc87c2d47
xopi <xflorimont@gmail.com>
2014-03-01 16:24:56 +0100

    Add ComponentType enum

---
+++ Classes/core/ComponentType.h
@@ -0,0 +1,19 @@
+/*!
+ * \file ComponentType.h
+ * \brief enum for each component type
+ * \author Xopi
+ * \date 26/02/2014
+ */
+#ifndef CORE_COMPONENT_TYPE_H
+#define CORE_COMPONENT_TYPE_H
+
+namespace core {
+typedef enum {
+	GEOMETRY,
+	MOVEMENT,
+	COLLISION,
+	HEROANIMATEDSPRITE
+} ComponentType;
+
+}  // namespace core
+#endif
