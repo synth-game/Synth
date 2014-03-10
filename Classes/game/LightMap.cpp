a5714a2b43e06003a81e41822362592441a9f275
Jijidici <jdefaye.jijidici@gmail.com>
2014-03-10 18:29:48 +0100

    Build LightMap from xml

--- Classes/game/LightMap.cpp
+++ Classes/game/LightMap.cpp
@@ -24,7 +24,41 @@ LightMap* LightMap::createFromXML(std::string sFilePath) {
 		
 		// load xml file
 		tinyxml2::XMLDocument doc;
-		doc.LoadFile(sFilePath.c_str());
+		int xmlerror = doc.LoadFile(sFilePath.c_str());
+		CCASSERT(xmlerror == 0, "ERROR WHILE LOADING LIGHTMAP XML FILE");
+
+		tinyxml2::XMLElement* pRootElt = doc.FirstChildElement("root");
+		pRet->_iW = pRootElt->IntAttribute("width");
+		pRet->_iH = pRootElt->IntAttribute("height");
+		pRet->_iResolutionCoef = pRootElt->IntAttribute("resolution_coef");
+
+		// initialize pixel map
+		pRet->_pixelGrid.resize(pRet->_iW*pRet->_iH);
+		Color4B noColor = Color4B::BLACK;
+		noColor.a = 0;
+		for(int i=0; i<pRet->_iW*pRet->_iH; ++i) {
+			pRet->_pixelGrid[i] = std::make_pair(noColor, std::vector<std::pair<int, bool>>());
+		}
+
+		// fill the pixel map
+		tinyxml2::XMLElement* pPixelElt = pRootElt->FirstChildElement("pixel");
+		while(pPixelElt != nullptr) {
+			int index = pPixelElt->IntAttribute("array_index");
+			std::vector<std::pair<int, bool>> lightSamples;
+
+			tinyxml2::XMLElement* pLightSampleElt = pPixelElt->FirstChildElement("light_sample");
+			while(pLightSampleElt != nullptr) {
+				std::pair<int, bool> sample;
+				sample.first = pLightSampleElt->IntAttribute("id");
+				sample.second = pLightSampleElt->BoolAttribute("occulted");
+				lightSamples.push_back(sample);
+
+				pLightSampleElt = pLightSampleElt->NextSiblingElement("light_sample");
+			}
+			pRet->_pixelGrid[index].second = lightSamples;
+
+			pPixelElt = pPixelElt->NextSiblingElement("pixel");
+		}
 
     } else {
         CC_SAFE_DELETE(pRet);
