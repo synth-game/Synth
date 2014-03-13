cf8cff658debd65b891331dff6b9cacaf9409e5e
Chupee <julie.pronzac@gmail.com>
2014-03-13 18:34:16 +0100

    Clean SynthConfig h and cpp

--- Classes/core/SynthConfig.cpp
+++ Classes/core/SynthConfig.cpp
@@ -1,82 +1,77 @@
+/*!
+ * \file SynthConfig.cpp
+ * \brief Singleton managing with the game config
+ * \author Chupee
+ * \date 13/03/2014
+ */
 #include "SynthConfig.h"
 
-namespace core
-{
+namespace core {
 
-SynthConfig::SynthConfig()
-{
+static SynthConfig* _pInstance = nullptr;
+
+SynthConfig::SynthConfig() {
 }
 
-SynthConfig::~SynthConfig()
-{
+SynthConfig::~SynthConfig() {
 }
 
-SynthConfig* SynthConfig::getInstance()
-{
-	return 0;
+SynthConfig* SynthConfig::getInstance() {
+	if(_pInstance == nullptr) {
+        _pInstance = new SynthConfig();
+    }
+	return _pInstance;
 }
 
-void SynthConfig::init(core::xml data)
-{
+void SynthConfig::init() {
+	
 }
 
-xml SynthConfig::getConfig()
-{
-	xml fakeXml;
+tinyxml2::XMLDocument* SynthConfig::getConfig() {
+	tinyxml2::XMLDocument* fakeXml;
 	return fakeXml;
 }
 
-float SynthConfig::getMusicVolume()
-{
+float SynthConfig::getMusicVolume() {
 	return 0;
 }
 
-float SynthConfig::getSoundVolume()
-{
+float SynthConfig::getSoundVolume() {
 	return 0;
 }
 
-Language SynthConfig::getLanguage()
-{
+Language SynthConfig::getLanguage() {
 	return Language::FRENCH;
 }
 
-Language SynthConfig::getSubtitleLanguage()
-{
+Language SynthConfig::getSubtitleLanguage() {
 	return Language::FRENCH;
 }
 
-float SynthConfig::getLuminosity()
-{
+float SynthConfig::getLuminosity() {
 	return 0;
 }
 
-int SynthConfig::getSavedLevel()
-{
+int SynthConfig::getSavedLevel() {
 	return 0;
 }
 
-void SynthConfig::setMusicVolume(float musicVolume)
-{
+void SynthConfig::setMusicVolume(float musicVolume) {
 }
 
-void SynthConfig::setSoundVolume(float soundVolume)
-{
+void SynthConfig::setSoundVolume(float soundVolume) {
 }
 
-void SynthConfig::setLanguage(Language eLanguage)
-{
+void SynthConfig::setLanguage(Language eLanguage) {
 }
 
-void SynthConfig::setSubtitleLanguage(Language eSubtitleLanguage)
-{
+void SynthConfig::setSubtitleLanguage(Language eSubtitleLanguage) {
 }
 
-void SynthConfig::setLuminosity(float fLuminosity)
-{
+void SynthConfig::setLuminosity(float fLuminosity) {
 }
 
-void SynthConfig::setSavedLevel(int iSavedLevel)
-{
+void SynthConfig::setSavedLevel(int iSavedLevel) {
 }
+
 }  // namespace core
