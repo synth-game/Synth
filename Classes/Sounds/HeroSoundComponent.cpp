aa2ce3336ff9fba09086868f0dc917f4e29ba6ca
Chupee <julie.pronzac@gmail.com>
2014-03-13 11:21:47 +0100

    Clean HeroSoundComponent .h and cpp

--- Classes/Sounds/HeroSoundComponent.cpp
+++ Classes/Sounds/HeroSoundComponent.cpp
@@ -1,42 +1,54 @@
+/*!
+ * \file HeroSoundComponent.cpp
+ * \brief The hero can make sounds !
+ * \author Chupee
+ * \date 13/03/2014
+ */
+
 #include "HeroSoundComponent.h"
 
-namespace sounds
-{
+#include "events/JumpEvent.h"
 
-HeroSoundComponent::HeroSoundComponent()
-{
+namespace sounds {
+
+HeroSoundComponent::HeroSoundComponent() :
+	SoundComponent() {
 }
 
-HeroSoundComponent::~HeroSoundComponent()
-{
+HeroSoundComponent::~HeroSoundComponent() {
 }
 
-HeroSoundComponent* HeroSoundComponent::create()
-{
-	return 0;
+HeroSoundComponent* HeroSoundComponent::create() {
+	HeroSoundComponent* pRet = new HeroSoundComponent();
+    if (pRet != NULL && pRet->init()) {
+        pRet->autorelease();
+    } else {
+        CC_SAFE_DELETE(pRet);
+    }
+	return pRet;
 }
 
-void HeroSoundComponent::initListeners()
-{
+void HeroSoundComponent::initListeners() {
+	// Listeners initialization
+	_pJumpEventListener = cocos2d::EventListenerCustom::create(events::JumpEvent::EVENT_NAME, CC_CALLBACK_1(HeroSoundComponent::onJump, this));
+
+	// Add listeners to dispacher
+	EventDispatcher::getInstance()->addEventListenerWithFixedPriority(_pJumpEventListener, 1);
 }
 
-void HeroSoundComponent::onChangePosition(EventCustom* pEvent)
-{
+void HeroSoundComponent::onChangePosition(EventCustom* pEvent) {
 }
 
-void HeroSoundComponent::onJump(EventCustom* pEvent)
-{
+void HeroSoundComponent::onJump(EventCustom* pEvent) {
 }
 
-void HeroSoundComponent::onInterruptMove(EventCustom* pEvent)
-{
+void HeroSoundComponent::onInterruptMove(EventCustom* pEvent) {
 }
 
-void HeroSoundComponent::onHeroDeath(EventCustom* pEvent)
-{
+void HeroSoundComponent::onHeroDeath(EventCustom* pEvent) {
 }
 
-void HeroSoundComponent::onPeriodicEvent(EventCustom* pEvent)
-{
+void HeroSoundComponent::onPeriodicEvent(EventCustom* pEvent) {
 }
+
 }  // namespace sounds
