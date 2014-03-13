16a252a9bf17437e9e3db09c95c501fb9932b33f
xopi <xflorimont@gmail.com>
2014-03-13 14:31:33 +0100

    Add constructor

--- Classes/events/ResetLevelEvent.cpp
+++ Classes/events/ResetLevelEvent.cpp
@@ -1,10 +1,17 @@
+/*!
+ * \file ResetLevelEvent.cpp
+ * \brief Event detecting when the level has to reset
+ * \author Xopi
+ * \date 13/03/2014
+ */
 #include "ResetLevelEvent.h"
 
 namespace events {
 
 const char* ResetLevelEvent::EVENT_NAME = "ResetLevelEvent";
 
-ResetLevelEvent::ResetLevelEvent() {
+ResetLevelEvent::ResetLevelEvent()
+	: SynthEvent(nullptr, EVENT_NAME) {
 }
 
 ResetLevelEvent::~ResetLevelEvent() {
--- Classes/events/ResetLevelEvent.h
+++ Classes/events/ResetLevelEvent.h
@@ -1,10 +1,16 @@
+/*!
+ * \file ResetLevelEvent.h
+ * \brief Event detecting when the level has to reset
+ * \author Xopi
+ * \date 13/03/2014
+ */
 #ifndef EVENTS_GAMEEVENTS_RESET_LEVEL_EVENT_H
 #define EVENTS_GAMEEVENTS_RESET_LEVEL_EVENT_H
 
 #include "Events/SynthEvent.h"
 
 namespace events {
-
+/*! \class ResetLevelEvent */
 class ResetLevelEvent : public events::SynthEvent {
 
 public:
