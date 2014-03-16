c58a510b8601cff76b3098f613eeb6595021200c
Chupee <julie.pronzac@gmail.com>
2014-03-16 16:09:26 +0100

    Clean Bounce collision component

--- Classes/physics/BounceCollisionComponent.cpp
+++ Classes/physics/BounceCollisionComponent.cpp
@@ -1,9 +1,14 @@
+/*!
+ * \file BounceCollisionComponent.h
+ * \brief The actor bounces on the floor
+ * \author Chupee
+ * \date 16/03/2014
+ */
 #include "BounceCollisionComponent.h"
 
-namespace physics
-{
+namespace physics {
 
-void BounceCollisionComponent::onTestCollision(EventCustom* pEvent)
-{
+void BounceCollisionComponent::onTestCollision(EventCustom* pEvent) {
 }
+
 }  // namespace physics
--- Classes/physics/BounceCollisionComponent.h
+++ Classes/physics/BounceCollisionComponent.h
@@ -1,13 +1,18 @@
+/*!
+ * \file BounceCollisionComponent.h
+ * \brief The actor bounces on the floor
+ * \author Chupee
+ * \date 16/03/2014
+ */
 #ifndef PHYSICS_BOUNCE_COLLISION_COMPONENT_H
 #define PHYSICS_BOUNCE_COLLISION_COMPONENT_H
 
 
 #include "Physics/CollisionComponent.h"
 
-namespace physics
-{
-class BounceCollisionComponent : public CollisionComponent
-{
+namespace physics {
+class BounceCollisionComponent : public CollisionComponent {
+
 public:
 	/**
 	 * si un point de la bounding box collisionne un mur, changer la vitesse du MovementComponent dans la direction inverse
