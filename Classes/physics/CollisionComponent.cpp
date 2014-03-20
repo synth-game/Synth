a58ae22d904b543488121146b4fd60ebf5bf528d
Jijidici <jdefaye.jijidici@gmail.com>
2014-03-20 14:34:14 +0100

    Improve collisions for skinning

--- Classes/physics/CollisionComponent.cpp
+++ Classes/physics/CollisionComponent.cpp
@@ -188,7 +188,7 @@ CollisionComponent::ECollisionType CollisionComponent::boundingTest(events::Test
 			// test bottom point first, if ONLY one of them collide, stick the actor on the ground (XOR operator)
 			if ((!_pPhysicCollision->collide(blPos) && fBLSampleCount >= 0.f) != (!_pPhysicCollision->collide(brPos) && fBRSampleCount >= 0.f)
 				&& _pPhysicCollision->collide(bcPos + movementStep*fBigBCSampleCount + Point(0., -1))) { // test if there is void below the moved bc pixel) {
-					fStepCountToExecute = fBigBCSampleCount;
+					fStepCountToExecute = std::min(fBigBCSampleCount, fMovementLength);
 					bIsOnGround = true;
 			} else {
 
